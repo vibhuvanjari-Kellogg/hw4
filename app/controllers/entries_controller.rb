e9c482ff60cd228bf198e448fb3d94855b44119f
Vibhu Vanjari <vibhu.vanjari@kellogg.northwestern.edu>
2025-03-14 05:55:06 +0000

    added ability to upload images

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -11,7 +11,9 @@ class EntriesController < ApplicationController
     @entry["occurred_on"] = params["occurred_on"]
     @entry["place_id"] = params["place_id"]
     @entry["user_id"] = session["user_id"]
+    @entry.uploaded_image.attach(params["uploaded_image"])
     @entry.save
+
     redirect_to "/places/#{@entry["place_id"]}"
   end
 
